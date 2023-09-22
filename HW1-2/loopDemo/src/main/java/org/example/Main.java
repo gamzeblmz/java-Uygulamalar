89399072ca5734f7ebfa1d45f5be91fb72964c5c
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-23 02:09:22 +0300

    for loop

---
+++ HW1-2/loopDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+        System.out.println("Loop with for is end");
+    }
+}
