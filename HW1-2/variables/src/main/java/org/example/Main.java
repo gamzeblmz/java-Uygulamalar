f81f87ab078141e8f63008c50aba273c26a07d45
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-23 01:38:49 +0300

    variables

---
+++ HW1-2/variables/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 10;
+        String mesaj = "ögrenci sayisi: ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("ögrenci sayisi: " + ogrenciSayisi);
+        System.out.println("ögrenci sayisi: " + ogrenciSayisi);
+
+    }
+}
