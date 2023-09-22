f96bda1b198ddf94840fd0ac59bc3e93c3d8be84
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-23 02:06:00 +0300

    switch blok

---
+++ HW1-2/switchDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        char grade = 'B';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Well done : You passed");
+                break;
+            case 'B':
+                System.out.println("Good job : You passed");
+                break;
+            case 'C':
+                System.out.println("Not bad : You passed");
+                break;
+            case 'F':
+                System.out.println("Unfortunately : You couldn't pass");
+                break;
+            default:
+                System.out.println("Invalid grade");
+
+        }
+    }
+}
