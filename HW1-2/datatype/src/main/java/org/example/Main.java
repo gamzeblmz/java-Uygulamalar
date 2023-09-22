5065bfa20eb858ad796f9d64b2c9b5d0d9996de7
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-23 01:45:22 +0300

    datatypes check

---
+++ HW1-2/datatype/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi=122;
+        double sayi2=3.5;
+        char karakter = 'G';
+        String sehir = "Eskisehir";
+        boolean dogru= true;
+        boolean yanlis= false;
+
+        System.out.println(sayi);
+        System.out.println(sayi2);
+        System.out.println(sehir);
+        System.out.println(karakter);
+        System.out.println(dogru);
+        System.out.println(yanlis);
+    }
+}
