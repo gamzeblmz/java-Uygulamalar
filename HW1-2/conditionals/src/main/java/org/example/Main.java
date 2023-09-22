de7e2b4f40277b08b7a08e21e7c02f40bd6ac2e0
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-23 01:51:16 +0300

    if conditions

---
+++ HW1-2/conditionals/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       int sayi =20;
+       if(sayi<20){
+           System.out.println("sayi 20 den kücüktür");
+       }
+
+        if(sayi<15){
+            System.out.println("sayi 15 den kücüktür");
+        }
+    }
+}
