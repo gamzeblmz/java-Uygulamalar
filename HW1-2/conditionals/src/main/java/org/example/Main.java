a2155be3d5fd13b0897511bae44915f12e3dec53
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-23 01:54:05 +0300

    elseif else

--- HW1-2/conditionals/src/main/java/org/example/Main.java
+++ HW1-2/conditionals/src/main/java/org/example/Main.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-       int sayi =20;
+       int sayi =24;
        if(sayi<20){
            System.out.println("sayi 20 den kücüktür");
+       } else if (sayi==20) {
+           System.out.println("sayi 20 ye esittir");
+       } else{
+           System.out.println("sayi 20 den kücük degildir");
        }
-
-        if(sayi<15){
-            System.out.println("sayi 15 den kücüktür");
-        }
     }
 }
