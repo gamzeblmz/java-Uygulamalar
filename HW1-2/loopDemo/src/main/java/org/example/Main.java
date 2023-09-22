df42b06dadcf48f521c9b4c571df459bb417114b
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-23 02:10:16 +0300

    while loop

--- HW1-2/loopDemo/src/main/java/org/example/Main.java
+++ HW1-2/loopDemo/src/main/java/org/example/Main.java
@@ -7,8 +7,15 @@ public class Main {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
-
         }
         System.out.println("Loop with for is end");
+
+        int i = 1;
+        while (i < 5) {
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("while loop is end");
+
     }
 }
