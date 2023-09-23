eaf509e169b87d2830cd2116f75a4a15e1352e04
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 02:37:54 +0300

    work with class

---
+++ HW2-2/classes/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //referance type
+        CustomerManager customerManager= new CustomerManager();
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+
+    }
+}
