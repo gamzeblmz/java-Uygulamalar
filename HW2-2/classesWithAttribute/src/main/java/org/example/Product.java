2035851931229c09ede8849a976882716c010602
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 02:59:10 +0300

    work with Field ve Attribute

---
+++ HW2-2/classesWithAttribute/src/main/java/org/example/Product.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Product {
+    //attribute | field
+    int id;
+    String name;
+    String description;
+    double price;
+    int stockAmount;
+    String renk;
+    String kod;
+
+
+}
