be578ac183f8ae69314583ad23a1a8525f5291d3
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 02:33:55 +0300

    add CustomerManager

---
+++ HW2-2/classes/src/main/java/org/example/CustomerManager.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Müsteri eklendi");
+    }
+    public void Remove(){
+        System.out.println("Müşteri silindi");
+    }public void Update(){
+        System.out.println("Müşteri guncellendi");
+    }
+}
