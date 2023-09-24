a64a1b4935119f9ffebf16fd486f62d902e17ed5
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 18:10:56 +0300

    cerate classes

---
+++ HW3-2/staticDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductManager productManager = new ProductManager();
+        Product product = new Product();
+        product.id = 1;
+        product.name = "mouse";
+        productManager.add(product);
+    }
+}
