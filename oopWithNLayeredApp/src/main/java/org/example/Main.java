17f776e00901b72f82270843b7967e46f6536864
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 20:43:29 +0300

    add product

--- oopWithNLayeredApp/src/main/java/org/example/Main.java
+++ oopWithNLayeredApp/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.business.ProductManager;
+import org.example.entities.Product;
+
 public class Main {
-    public static void main(String[] args) {
-      
+    public static void main(String[] args) throws Exception {
+        Product product = new Product(1, "Iphone xr", 8);
+        ProductManager productManager = new ProductManager();
+        productManager.add(product);
+
+
     }
 }
--- oopWithNLayeredApp/src/main/java/org/example/business/ProductManager.java
+++ oopWithNLayeredApp/src/main/java/org/example/business/ProductManager.java
@@ -1,9 +1,15 @@
 package org.example.business;
 
+import org.example.dataAccess.JdbcProductDao;
 import org.example.entities.Product;
 
 public class ProductManager {
-    public void add(Product product){
-
+    JdbcProductDao jdbcProductDao= new JdbcProductDao();
+    public void add(Product product) throws Exception {
+        // is kurallari yazilir
+        if(product.getUnitPrice()<10){
+           throw new Exception("urun fiyati 10'dan kucuk olamaz.");
+        }
+        jdbcProductDao.add(product);
     }
 }
