0feeb3f0b305aa2c0f5a18bc67cc1a5823a62791
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 04:10:41 +0300

    final keyword

--- HW3-2/overriding/src/main/java/org/example/BaseKrediManager.java
+++ HW3-2/overriding/src/main/java/org/example/BaseKrediManager.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class BaseKrediManager {
+    //final keywordü ile yazilirsa method ezilemez override edilemez yani!!!!!
     public double hesapla(double tutar){
         return tutar * 1.18;
     }
