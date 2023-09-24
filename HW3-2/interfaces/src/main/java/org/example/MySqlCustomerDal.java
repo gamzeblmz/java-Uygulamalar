b43cc6e609abf94a62a03a62c032886f87b2d8f0
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 16:32:08 +0300

    impelement interfaces

--- HW3-2/interfaces/src/main/java/org/example/MySqlCustomerDal.java
+++ HW3-2/interfaces/src/main/java/org/example/MySqlCustomerDal.java
@@ -1,6 +1,7 @@
 package org.example;
 
-public class MySqlCustomerDal implements ICustomerDal{
+//bir class birden fazla interface implement edebilir
+public class MySqlCustomerDal implements ICustomerDal,IRepository{
     @Override
     public void add() {
         System.out.println("MySql eklendi.");
