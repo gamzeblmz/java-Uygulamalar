8315fdc4af182937bf995c7ce4d87394d8795902
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 03:41:16 +0300

    inheritance demo

---
+++ HW2-2/inheritanceDemo/src/main/java/org/example/KrediUI.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class KrediUI {
+    public void krediHesapla(BaseKrediManager baseKrediManager ){
+        baseKrediManager.Hesapla();
+    }
+}
--- HW2-2/inheritanceDemo/src/main/java/org/example/Main.java
+++ HW2-2/inheritanceDemo/src/main/java/org/example/Main.java
@@ -2,9 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
- OgretmenKrediManager ogretmenKrediManager=new OgretmenKrediManager();
-       ogretmenKrediManager.Hesapla();
-
+        KrediUI krediUI=new KrediUI();
+        krediUI.krediHesapla(new AskerKrediManager());
 
     }
 }
