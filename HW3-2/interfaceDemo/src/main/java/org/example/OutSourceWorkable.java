0728a123811d1b46e778711805856d7f125eabfb
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 18:02:02 +0300

    Multiple Implementations

---
+++ HW3-2/interfaceDemo/src/main/java/org/example/OutSourceWorkable.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class OutSourceWorkable implements IWorkable{
+    @Override
+    public void work() {
+
+    }
+}
---
+++ HW3-2/interfaceDemo/src/main/java/org/example/Worker.java
@@ -0,0 +1,20 @@
+package org.example;
+
+//bizim çalısanimiz
+public class Worker implements IWorkable ,IEatable,IPayable{
+
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void pay() {
+
+    }
+}
