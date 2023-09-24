4674626cf38a4724284bb694a9315e1e06d11b04
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 18:43:23 +0300

    static methods

--- HW3-2/staticDemo/src/main/java/org/example/ProductManager.java
+++ HW3-2/staticDemo/src/main/java/org/example/ProductManager.java
@@ -1,12 +1,20 @@
 package org.example;
 
 public class ProductManager {
-    public void add(Product product){
-        ProductValidator productValidator= new ProductValidator();
-        if(productValidator.isValid(product)){
+    public void add(Product product) {
+        ProductValidator productValidator = new ProductValidator();
+//        if(productValidator.isValid(product)){
+//            System.out.println("eklendi.");
+//        }else {
+//            System.out.println("ürün bilgileri gecersizdir.");
+//        }
+
+        // static ifadesi nesnenin örneğini bir kez olusturur
+        if (ProductValidator.isValid(product)) {
             System.out.println("eklendi.");
-        }else {
+        } else {
             System.out.println("ürün bilgileri gecersizdir.");
         }
+
     }
 }
--- HW3-2/staticDemo/src/main/java/org/example/ProductValidator.java
+++ HW3-2/staticDemo/src/main/java/org/example/ProductValidator.java
@@ -1,10 +1,26 @@
 package org.example;
 
 public class ProductValidator {
-    public boolean isValid(Product product) {
+    //const newleyince calisir
+    public ProductValidator() {
+        System.out.println("yapici blok calisti");
+    }
+    static {
+        System.out.println("satic method calisti.");
+    }
+    public static boolean isValid(Product product) {
         if (product.price > 0 && !product.name.isEmpty()) {
             return true;
         } else
             return false;
+    }
+
+    //method static olmadıgı için çağırırken newlemek gerekir
+    public void birsey() {
+
+    }
+    //ana class static olmaz ama bu sekilde kullanılır buna inner class denir
+    public static class BaskaBirClass{
+
     }
 }
