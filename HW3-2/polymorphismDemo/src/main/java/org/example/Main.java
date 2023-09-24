817b884149cd1d38c879c12227eb7c76d2695d61
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 03:58:53 +0300

    polymorphism

--- HW3-2/polymorphismDemo/src/main/java/org/example/Main.java
+++ HW3-2/polymorphismDemo/src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
-       //EmailLogger emailLogger=new EmailLogger();
-      // emailLogger.log("email");
-       BaseLogger[] loggers = new BaseLogger[]{new FileLogger(),new DatabaseLogger(),new EmailLogger()};
-       for (BaseLogger logger:loggers){
-           logger.log("Log message");
-       }
+        //EmailLogger emailLogger=new EmailLogger();
+        // emailLogger.log("email");
+
+//       BaseLogger[] loggers = new BaseLogger[]{new FileLogger(),new DatabaseLogger(),new EmailLogger()};
+//       for (BaseLogger logger:loggers){
+//           logger.log("Log message");
+//       }
+
+
+        CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
+        customerManager.add();
     }
 }
