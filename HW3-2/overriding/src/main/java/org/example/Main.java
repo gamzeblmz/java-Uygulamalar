35bfecd8f866c179a0ddffffe6abfdde8768cce4
gamze.bilmez <gamzebilmezz@gmail.com>
2023-09-24 04:08:16 +0300

    overriding

--- HW3-2/overriding/src/main/java/org/example/Main.java
+++ HW3-2/overriding/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
           BaseKrediManager[] baseKrediManager=new BaseKrediManager[]{
-                  new TarimKrediManager(),new OgretmenKrediManager()};
+                  new TarimKrediManager(),new OgretmenKrediManager(),new OgrenciKrediManager()};
           for (BaseKrediManager baseKrediManager1:baseKrediManager){
               System.out.println(baseKrediManager1.hesapla(1000));
           }
